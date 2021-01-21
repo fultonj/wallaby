24de1109b9338f5bcdbe33ece07c6b5b5b9de50e
John Fulton <fulton@redhat.com>
2021-01-21 16:22:48 +0000

    Make git refresh pull in stacked patch
    Patch 2 is based on patch 1.
    
    1. 770674: Introduce tripleo_cephadm role
    2. 771034: Introduce tripleo_run_cephadm role and cephadm playbook
    
    Pull in the latest of both but pull in patch 2 after patch 1 so
    that I ended up default with patch 2 ready to run. I might have
    to manually rebase as I introduce changes, but when I need to
    make changes to 1, the latest copy will be ready. Do not delete
    the older branch so that I can have both to work on. git review
    is smart enough to re-use and update without the need to delete
    the old branch.

--- init/refresh.sh
+++ init/refresh.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 declare -A REPO_MAP=(
-         [767294]='tripleo-heat-templates' \
-         [771034]='tripleo-ansible' \
-        );
+                     [767294]='tripleo-heat-templates' \
+                     [770674]='tripleo-ansible' \
+                     [771034]='tripleo-ansible' \
+                    );
 
 for K in "${!REPO_MAP[@]}"; do
     pushd ~/${REPO_MAP[$K]}
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
     git reset --hard
     git checkout master
-    git branch -D $BRANCH
+    #git branch -D $BRANCH
     git review -d $K
     git branch
     popd
