ba3b7028c72a1f6d2050f57196285fcf0b66bc42
John Fulton <fulton@redhat.com>
2021-01-25 22:16:23 +0000

    Add quick/dirty playbook to undeploy ceph without affecting hardware

---
+++ external/rm_ceph.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STACK=oc0
+if [[ -e inventory.yaml ]]; then
+    INV=inventory.yaml
+elif [[ -e ~/config-download/$STACK/cephadm/inventory.yml ]]; then
+    INV=~/config-download/$STACK/cephadm/inventory.yml
+else
+    echo "missing inventory"
+    exit 1
+fi
+echo $INV
+
+ansible-playbook -i $INV rm_ceph.yaml $@
