32ea00fd0057f46ccb2ce6aee8d04db81b5238b4
John Fulton <fulton@redhat.com>
2021-01-27 21:26:13 +0000

    put old scripts in external/utilities

--- external/utilities/rm_ceph.sh
+++ external/utilities/rm_ceph.sh
@@ -2,7 +2,7 @@
 
 STACK=oc0
 if [[ -e inventory.yaml ]]; then
-    INV=inventory.yaml
+    INV=../inventory.yaml
 elif [[ -e ~/config-download/$STACK/cephadm/inventory.yml ]]; then
     INV=~/config-download/$STACK/cephadm/inventory.yml
 else
