c96cddbd8d6b574f0ab4073d98c03419cf7a1dd5
John Fulton <fulton@redhat.com>
2021-03-31 17:06:48 +0000

    standalone without generated-container-prepare
    I'm testing [1] with standalone so I don't want to pass
    a large list of many possible containers. Instead, I
    want to see the change [1] genereate the appropriate
    list of containers on the fly relative to what is needed.
    
    [1] https://review.opendev.org/c/openstack/tripleo-common/+/782009

--- standalone/standalone.sh
+++ standalone/standalone.sh
@@ -4,12 +4,21 @@ if [[ ! -d ~/templates ]]; then
     ln -s /usr/share/openstack-tripleo-heat-templates ~/templates
 fi
 
+CONTAINER_FILE=/home/stack/containers-prepare-parameters.yaml
+if [[ ! -e $CONTAINER_FILE ]]; then
+    # need sudo to write to /var/lib/image-serve
+    sudo openstack tripleo container image prepare default \
+         --local-push-destination \
+         --output-env-file $CONTAINER_FILE
+    sudo chown stack:stack $CONTAINER_FILE
+fi
+
 sudo openstack tripleo deploy \
   --templates ~/templates \
   --local-ip=192.168.23.1/24 \
   -e ~/templates/environments/standalone/standalone-tripleo.yaml \
   -r ~/templates/roles/Standalone.yaml \
-  -e ~/generated-container-prepare.yaml \
+  -e $CONTAINER_FILE \
   -e ~/standalone_parameters.yaml \
   --output-dir $HOME \
   --standalone $@
