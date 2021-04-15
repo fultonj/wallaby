e98cb783380dc63fb7b42560cbde7f38f24cc665
John Fulton <fulton@redhat.com>
2021-04-15 17:45:32 +0000

    add standalone deploy and clenaup scripts

---
+++ standalone/virsh/cleanup.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+echo "Tearing down Ceph environment"
+
+# stop and disable ceph containers
+for N in mgr mon osd; do
+    if [[ -e /etc/systemd/system/ceph-${N}@.service ]]; then
+        sudo systemctl stop ceph-${N}@*
+        sudo systemctl disable ceph-${N}@
+        sudo rm -f /etc/systemd/system/ceph-${N}@.service
+    fi
+done
+
+# remove ceph container image
+for IMG in $(sudo podman images \
+                  --format "{{.ID}} {{.Repository}}" \
+                 | grep ceph | awk {'print $1'} ); do
+    sudo podman rmi $IMG;
+done
+
+# remove ceph directories
+sudo rm -rf \
+     /var/log/ceph \
+     /var/run/ceph \
+     /var/lib/ceph \
+     /run/ceph \
+     /etc/ceph/*
+
+# remove the secret key of the openstack client from libvirt
+for pkg in libvirt-client; do
+    rpm -q $pkg > /dev/null
+    if [[ $? -ne 0 ]]; then
+        sudo yum install -y libvirt-client
+    fi
+done
+for S in $(sudo virsh -q secret-list | awk {'print $1'}); do
+    sudo virsh secret-undefine $S
+done
+sudo find / -name secret.xml -exec rm -f {} \; 2> /dev/null
+
+# remove the disk used by ceph
+sudo lvremove --force /dev/vg2/db-lv2
+sudo lvremove --force /dev/vg2/data-lv2
+sudo vgremove --force vg2
+sudo pvremove --force /dev/loop3
+sudo losetup -d /dev/loop3
+sudo rm -f /var/lib/ceph-osd.img
+sudo partprobe
+
+
+
+echo "Tearing down TripleO environment"
+if type pcs &> /dev/null; then
+    sudo pcs cluster destroy
+fi
+if type podman &> /dev/null; then
+    echo "Removing podman containers and images (takes times...)"
+    sudo podman rm -af
+    sudo podman rmi -af
+fi
+sudo rm -rf \
+    /var/lib/tripleo-config \
+    /var/lib/config-data /var/lib/container-config-scripts \
+    /var/lib/container-puppet \
+    /var/lib/heat-config \
+    /var/lib/image-serve \
+    /var/lib/containers \
+    /etc/systemd/system/tripleo* \
+    /var/lib/mysql/*
+sudo systemctl daemon-reload
+
---
+++ standalone/virsh/standalone.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+export INTERFACE=eth0
+export IP=192.168.24.2
+export VIP=192.168.24.3
+export NETMASK=24
+export DNS_SERVERS=192.168.122.1
+export NTP_SERVERS=pool.ntp.org
+
+cat <<EOF > $HOME/standalone_parameters.yaml
+parameter_defaults:
+  CloudName: $IP
+  ControlPlaneStaticRoutes: []
+  Debug: true
+  DeploymentUser: $USER
+  DnsServers: $DNS_SERVERS
+  NtpServer: $NTP_SERVERS
+  # needed for vip & pacemaker
+  KernelIpNonLocalBind: 1
+  DockerInsecureRegistryAddress:
+  - $IP:8787
+  NeutronPublicInterface: $INTERFACE
+  # domain name used by the host
+  NeutronDnsDomain: localdomain
+  # re-use ctlplane bridge for public net
+  NeutronBridgeMappings: datacentre:br-ctlplane
+  NeutronPhysicalBridge: br-ctlplane
+  # enable to force metadata for public net
+  #NeutronEnableForceMetadata: true
+  StandaloneEnableRoutedNetworks: false
+  StandaloneHomeDir: $HOME
+  InterfaceLocalMtu: 1500
+  # Needed if running in a VM
+  NovaComputeLibvirtType: qemu
+EOF
+
+if [ ! -f $HOME/ceph_parameters.yaml ]; then
+  cat <<EOF > $HOME/ceph_parameters.yaml
+parameter_defaults:
+  CephAnsibleDisksConfig:
+    osd_scenario: lvm
+    osd_objectstore: bluestore
+    lvm_volumes:
+      - data: data-lv2
+        data_vg: vg2
+        db: db-lv2
+        db_vg: vg2
+        wal: wal-lv2
+        wal_vg: vg2
+  CephAnsibleExtraConfig:
+    cluster_network: 192.168.24.0/24
+    public_network: 192.168.24.0/24
+  CephPoolDefaultPgNum: 32
+  CephPoolDefaultSize: 1
+EOF
+fi
+
+if [[ ! -d ~/templates ]]; then
+    ln -s /usr/share/openstack-tripleo-heat-templates ~/templates
+fi
+
+# sudo pkill -9 heat-all # Remove any previously running heat processes
+sudo openstack tripleo deploy \
+  --templates ~/templates \
+  --local-ip=$IP/$NETMASK \
+  --control-virtual-ip $VIP \
+  -e ~/templates/environments/standalone/standalone-tripleo.yaml \
+  -e ~/templates/environments/ceph-ansible/ceph-ansible.yaml \
+  -r ~/templates/roles/Standalone.yaml \
+  -e ~/containers-prepare-parameters.yaml \
+  -e ~/standalone_parameters.yaml \
+  -e ~/ceph_parameters.yaml \
+  --output-dir $HOME \
+  --standalone $@
+
+
+#  --keep-running \
